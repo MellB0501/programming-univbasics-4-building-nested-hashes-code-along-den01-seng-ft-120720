703ff08093dff7dba3742c100ff9a948d70c65d0
Mellissa Basham <mellbasham@gmail.com>
2020-11-22 03:30:35 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,9 +10,9 @@ epic_tragedy = {
     ]
 },
 :capulet => {
-  :patriarch => "Lord Capulet", age: "50"},
-  :matriarch => "Lady Capulet", age "51"},
-  :heroine => "Juliet", age "15", status: "alive"},
+  :patriarch => name: "Lord Capulet", age: "50"},
+  :matriarch => name: "Lady Capulet", age "51"},
+  :heroine => name: "Juliet", age "15", status: "alive"},
   :heroine_friends => [
     {name: "Steven", age: "30", attitude: "confused"},
     {name: "Nurse", age: "44", attitude: "worried"}
